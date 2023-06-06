0ae1397a7be88f3f2972c0f3b0bf83f502059c42
Luis Liquin <luisliquin@outlook.com.ar>
2023-06-06 19:12:27 -0300

    PreEntrega1 Liquin Luis

--- practicas/01Practica/iTechEmpire/src/App.jsx
+++ practicas/01Practica/iTechEmpire/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import ItenListContainer from './components/ItemListContainer/ItemListContainer'
+import 'materialize-css/dist/css/materialize.min.css'
 
 function App() {
   const [count, setCount] = useState(0)
--- practicas/01Practica/iTechEmpire/src/components/NavBar/NavBar.jsx
+++ practicas/01Practica/iTechEmpire/src/components/NavBar/NavBar.jsx
@@ -3,12 +3,14 @@ import CartWidget from "../CartWidget/CardWidget";
 const NavBar = () => {
     return(
         <>
-       <nav>
-          <h1>Ecommerce</h1>
-          <div>
-          <button>Celulares</button>
-          <button>Tablets</button>
-          <button>Notebooks</button>
+        <div>
+           <h1>Ecommerce</h1>
+        </div>
+       <nav class="orange darken-2">
+          <div class="nav-wrapper">
+            <button>Celulares</button>
+            <button>Tablets</button>
+            <button>Notebooks</button>
           </div>
        </nav>
        <CartWidget/>
