5d7b4ce7ded56d43a90cd5df4acb9fdea87eb2f2
Luis Liquin <luisliquin@outlook.com.ar>
2023-06-19 10:51:36 -0300

    clase after_06_07

---
+++ clases/07/src/App.jsx
@@ -0,0 +1,23 @@
+//import AsyncAwait from "./componentes/AsyncAwait/AsyncAwait"
+//import JSONPlace from "./componentes/JSONPlace/JSONPlace"
+//import Nesflic from "./componentes/Nesflic/Nesflic"
+import ContactForm from './componentes/ContactForm/ContactForm'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <ContactForm/>
+      
+
+
+
+      {/*
+      <Nesflic/>
+      <AsyncAwait/>
+      <JSONPlace/> */}
+    </div>
+  )
+}
+
+export default App
