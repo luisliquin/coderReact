5d19912c0026bb6612df999bb31da13dd0be8b29
Luis Liquin <luisliquin@outlook.com.ar>
2023-06-25 00:39:34 -0300

    tp 2

--- practicas/02Practica/iTechEmpire/src/App.jsx
+++ practicas/02Practica/iTechEmpire/src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar'
 import ItenListContainer from './components/ItemListContainer/ItemListContainer'
 import 'materialize-css/dist/css/materialize.min.css'
 import Footer from './components/Footer/Footer'
+import ItemCount from './components/itemCount/itemCount'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <> 
       <NavBar/>   
       <ItenListContainer greeting ={"Bienvenidos"}/>
+      <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log("Cantidad agregada:", quantity) }/>
       <Footer/>
     </>
   )
