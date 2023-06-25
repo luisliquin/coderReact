7b6a16e147c7c45deff5681995ed631fed73f1fe
Luis Liquin <luisliquin@outlook.com.ar>
2023-06-24 22:06:11 -0300

    correcciones

--- practicas/02Practica/iTechEmpire/src/components/NavBar/NavBar.jsx
+++ practicas/02Practica/iTechEmpire/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
 import CartWidget from "../CartWidget/CartWidget";
-import "./NavBar.css"
+import "./NavBar";
 
 const NavBar = () => {
   useEffect(() => {
