3fb5c14fb3c17ed307fb4506ffc261caaae61d32
Luis Liquin <luisliquin@outlook.com.ar>
2023-07-01 13:13:16 -0300

    changes tp 2

--- practicas/02Practica/iTechEmpire/src/App.jsx
+++ practicas/02Practica/iTechEmpire/src/App.jsx
@@ -1,19 +1,22 @@
 import './App.css'
-import NavBar from './components/NavBar/NavBar'
-import ItenListContainer from './components/ItemListContainer/ItemListContainer'
 import 'materialize-css/dist/css/materialize.min.css'
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import Footer from './components/Footer/Footer'
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 
 function App() {
-
   return (
     <> 
       <NavBar/>   
-      <ItenListContainer greeting ={"Bienvenidos"}/>
+      <ItemListContainer greeting ={"Bienvenidos"}/>
       <Router>
         <Routes>
-          <Route path='/'/>
+        <Route path="/" element={ <ItemListContainer/> } />
+          <Route path="/categoria/:idCategoria" element={ <ItemListContainer/> } />
+          <Route path="/item/:idItem" element={<ItemDetailContainer/>} />
+          <Route path="*" element={<h2>Sitio en construcción, vuelva más tarde</h2>}  />
         </Routes>
       </Router>
       <Footer/>
