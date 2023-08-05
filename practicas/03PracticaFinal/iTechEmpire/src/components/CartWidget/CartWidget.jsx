cd4c569de53c51ca3f85291102cb6f63cd5814d1
Luis Liquin <luisliquin@outlook.com.ar>
2023-08-05 12:09:12 -0300

    EntregaFinalLiquin

--- practicas/03PracticaFinal/iTechEmpire/src/components/CartWidget/CartWidget.jsx
+++ practicas/03PracticaFinal/iTechEmpire/src/components/CartWidget/CartWidget.jsx
@@ -1,12 +1,21 @@
 import logo from "../../assets/images/logo.svg"
 import './CartWidget.css'
+import { useContext } from 'react';
+import { CarritoContext } from '../../context/CarritoContext';
+import { Link } from 'react-router-dom';
 
 const CartWidget = () => {
+    const {cantidadTotal} = useContext(CarritoContext);
     return(
         <>
+            <Link to="/cart">  
             <img className='imgCarrito' src={logo} alt="cart-widget" />
-            <strong>10</strong>
+            {
+                cantidadTotal > 0 && <strong> {cantidadTotal} </strong>
+            }
+            </Link>
         </>
+
     )
 }
 
